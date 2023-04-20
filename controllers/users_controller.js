c81cf83b94b0d861f28b432730ec76762bbd0de4
Apurv <apurv.mod007@gmail.com>
2023-04-20 23:28:48 +0530

    added user sign-in and sign out view

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,5 +2,21 @@ module.exports.profile = function(req, res){
     // res.end('<h1>User Profile</h1>');
     return res.render('user_profile',{
         title:"profile"
+    
+    });
+
+}
+ 
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render = ('user_sign_up',{
+        title: "Codeial | Sign up"
+    });
+
+}
+//render the sign in page 
+module.exports.signIn = function(req,res){
+    return res.render = ('user_sign_in',{
+        title:"Codeial | Sign In"
     });
 }
