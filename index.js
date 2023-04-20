efb88963ac1f5f0b0bb368ca77312c69153ade09
Apurv <apurv.mod007@gmail.com>
2023-04-20 20:03:59 +0530

    setup static files access for layuts and pages

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 8000;
 
+
+//make static file
+app.use(express.static('./assets'));
 //use express router 
 
+app.use(expressEjsLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+//use express router
 app.use('/', require('./routes'));
+
+//set up the view engine 
+app.set('view engine', 'ejs');
+app.set('views','./views');
 
 app.listen(port, function(err){
     if (err){
