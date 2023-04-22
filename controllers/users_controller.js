ae2799535e252fe4f56fcf57c4d8d5b3318f3736
Apurv <apurv.mod007@gmail.com>
2023-04-22 16:32:54 +0530

    pushed data sign up data into database

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+
+
 module.exports.profile = function(req, res){
     // res.end('<h1>User Profile</h1>');
     return res.render('user_profile',{
@@ -9,14 +12,39 @@ module.exports.profile = function(req, res){
  
 //render the sign up page
 module.exports.signUp = function(req,res){
-    return res.render = ('user_sign_up',{
+    return res.render('user_sign_up',{
         title: "Codeial | Sign up"
     });
 
 }
 //render the sign in page 
 module.exports.signIn = function(req,res){
-    return res.render = ('user_sign_in',{
+    return res.render('user_sign_in',{
         title:"Codeial | Sign In"
     });
+}
+
+//get the sign up data 
+module.exports.create = async function(req, res) {
+    try {
+      if (req.body.password !== req.body.confirm_password) {
+        return res.redirect('back');
+      }
+  
+      const user = await User.findOne({ email: req.body.email });
+  
+      if (!user) {
+        const createdUser = await User.create(req.body);
+        return res.redirect('/users/sign-in');
+      } else {
+        return res.redirect('back');
+      }
+    } catch (err) {
+      console.log('Error in creating user while signing up', err);
+      return res.redirect('back');
+    }
+  };
+//sign in and create the session for the user
+module.exports.createSession = function(req,res){
+    //todo later 
 }
